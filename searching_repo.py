19226e30f9f4cf58a1a7ee8da67378ffedede613
joey-s1 <joey.chen@sentinelone.com>
2022-02-14 18:02:44 +0800

    update command version

--- searching_repo.py
+++ searching_repo.py
@@ -1,5 +1,5 @@
 from github import Github
-import gspread,datetime,re
+import gspread,datetime,re,argparse
 from oauth2client.service_account import ServiceAccountCredentials
 
 #Github Auth token
@@ -9,10 +9,10 @@ for token_ in token.readlines():
 g = Github(ACCESS_TOKEN)
 datetime_dt = datetime.datetime.today()
 
-def search_github(keywords):
+def search_github(keywords, day_):
     #time range
     datetime_str = datetime_dt.strftime("%Y-%m-%d")
-    delta_day = datetime.timedelta(days=7)    #defult is past a week
+    delta_day = datetime.timedelta(days=day_)    #defult is past a week
     early_datetime_str = datetime_dt - delta_day
     early_datetime_str_ = early_datetime_str.strftime("%Y-%m-%d")
     #query args
@@ -54,12 +54,31 @@ def gsheet(datas):
     for data in datas:
         worksheet.append_row(data)
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="exploit search")
+    parser.add_argument("type", choices=["gsheet", "print"],
+                        help="print out type")
+    parser.add_argument("-d", "--day", type=int, default=1,
+                        help="trace back day")
+    args = parser.parse_args()
+    return vars(args)
+
+def main(keywords):
+    args = parse_args()
+    if args["type"] == "gsheet":
+        repo_data = search_github(keywords, day_=args["day"])
+        gsheet(repo_data)
+    else:
+        repo_data = search_github(keywords, day_=args["day"])
+        for i in repo_data:
+            print(i, end = ' ')
+            print()
+
 if __name__ == '__main__':
     #Keyword for github search, ex:CVE number, exploit name, etc.
     fo = open("keywords.txt", "r")
     for keywords in fo.readlines(): 
         keywords = keywords.strip()       
     keywords = [keyword.strip() for keyword in keywords.split(',')]
+    main(keywords)
     
-    repo_data = search_github(keywords)
-    gsheet(repo_data)
